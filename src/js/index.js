c388c0e2dca6264c0bf8601d437e7af0161cfaba
Alan Lima <alan.silva1f@gmail.com>
2022-12-24 22:06:07 -0300

    Commintando projeto flags

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,10 @@
 const urlFlags = all => `https://restcountries.com/v3.1/${all}`;
+const urlRegion = region => `https://restcountries.com/v3.1/region/${region}`;
+const urlName = name => `https://restcountries.com/v3.1/name/${name}`
 
-const containerFlags = document.querySelector(".container_flags")
+const containerFlags = document.querySelector(".container_flags");
+const namesContinentes = document.querySelector("#names_continentes");
+
 const all = "all";
 
 const searchDateFlags = async flags =>{
@@ -11,8 +15,53 @@ const searchDateFlags = async flags =>{
 const printFlagOnCanvas =  async allFlags =>{
     let dateFlags = await searchDateFlags(allFlags);
 
+    containerFlags.innerHTML = "";
+
     let createFlags = dateFlags.map(flag =>{
-        console.log(flag.capital)
+        const { name, population, region, flags, capital } = flag;
+
+        return `
+        <div class="card_flag">
+            <div class="img_flag">
+                <img src="${flags.svg}" alt="${name.common}">
+            </div>
+    
+            <div class="description_flag">
+                <h3 class="name_flag">${name.common}</h3>
+                <ul class="about_flag">
+                    <li>
+                        <strong class="populatin">Population:</strong> ${Number.parseFloat(population)
+                                                                               .toLocaleString()}
+                    </li>
+                    <li>
+                        <strong class="region">Region:</strong> ${region}
+                    </li>
+                    <li>
+                        <strong class="capital">Capital:</strong> ${capital}
+                    </li>
+                </ul>
+            </div>
+        </div>`;
+    })
+
+    containerFlags.innerHTML += createFlags.join("")
+};
+
+const selectOfContinents = async event =>{
+    let selectedValue = event.target.value;
+
+    if(selectedValue === "all"){
+        printFlagOnCanvas(selectedValue);
+        return
+    }
+
+    let dataByRegion = await fetch(urlRegion(selectedValue))
+    let continentsAndTheirRegions = await dataByRegion.json();
+
+    containerFlags.innerHTML = " ";
+
+
+    let createFlags = continentsAndTheirRegions.map(flag =>{
         return `
         <div class="card_flag">
             <div class="img_flag">
@@ -39,9 +88,7 @@ const printFlagOnCanvas =  async allFlags =>{
     containerFlags.innerHTML += createFlags.join("")
 }
 
-
-
-
+namesContinentes.addEventListener("change", selectOfContinents);
 printFlagOnCanvas(all);
 
 
